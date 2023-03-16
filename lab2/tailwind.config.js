fe2221266a00ba2faa7d1833c50d4d9c44f08b0e
MephistoSolsist <1443735959@qq.com>
2023-03-16 23:55:21 +0800

    Improved Lab2

--- lab2/tailwind.config.js
+++ lab2/tailwind.config.js
@@ -2,7 +2,9 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: { 'black': { DEFAULT: '#000000', dark: '#000000' } }, 
+    },
   },
   plugins: [],
 }
