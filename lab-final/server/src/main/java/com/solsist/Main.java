f037472bfa52fb935212ecb4c588c428cc8b7232
Mephisto <1443735959@qq.com>
2023-06-28 11:18:41 +0800

    final done

--- lab-final/server/src/main/java/com/solsist/Main.java
+++
@@ -1,7 +0,0 @@
-package com.solsist;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(MusicApi.getMusicList());
-    }
-}
--- lab-final/server/src/main/java/com/solsist/mapper/MusicMapper.java
+++
@@ -1,9 +0,0 @@
-package com.solsist.mapper;
-
-import com.solsist.pojo.Music;
-
-import java.util.List;
-
-public interface MusicMapper {
-    List<Music> selectAll();
-}
